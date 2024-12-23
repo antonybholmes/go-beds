c224773dfcdc2847eaf164a7c97b6b237fd16322
Antony Holmes <antony@antonyholmes.dev>
2024-12-23 12:07:29 -0500

    fix: Bug fixes and updates.

--- step1_bedtosql.sh
+++ step1_bedtosql.sh
@@ -9,5 +9,5 @@ do
 	#bed=Peaks_CB4_BCL6_RK040_vs_Input_RK063_p12.bed
     	python bedtosql.py --sample=${sample} --bed=${bed} --genome=${genome} --out=${out}
     	./step2_create_db.sh ${out}  
-	break
+	#break
 done 
