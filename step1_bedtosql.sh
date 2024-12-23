caa3ffac9cb9082281fb073b731cf36e0230b01d
Antony Holmes <antony@antonybholmes.com>
2024-12-23 12:09:22 -0500

    fix: Bug fixes and updates.

--- step1_bedtosql.sh
+++ step1_bedtosql.sh
@@ -6,8 +6,8 @@ do
     	sample=`basename ${bed} | sed -r 's/\..+//' | sed -r 's/Peaks_//'` #Peaks_CB4_BCL6_RK040_vs_Input_RK063_p12
     	out=${dir}/${genome}/${sample}.sql 
     	echo ${sample} ${bed}
-	#bed=Peaks_CB4_BCL6_RK040_vs_Input_RK063_p12.bed
+	    #bed=Peaks_CB4_BCL6_RK040_vs_Input_RK063_p12.bed
     	python bedtosql.py --sample=${sample} --bed=${bed} --genome=${genome} --out=${out}
     	./step2_create_db.sh ${out}  
-	#break
+	    #break
 done 
