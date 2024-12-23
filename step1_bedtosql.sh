f44eb93c5696192c58996f7f6bbfbb5186f06637
Antony Holmes <antony@antonyholmes.dev>
2024-12-23 12:05:25 -0500

    fix: Bug fixes and updates.

--- step1_bedtosql.sh
+++ step1_bedtosql.sh
@@ -3,9 +3,11 @@ dir=data/modules/beds/ChIP-seq
 
 cat beds.txt | sed 1d | while read bed
 do
-    sample=`echo ${bed} | sed -r 's/\..+//'` #Peaks_CB4_BCL6_RK040_vs_Input_RK063_p12
-    out=${dir}/${genome}/${sample}.sql 
-    #bed=Peaks_CB4_BCL6_RK040_vs_Input_RK063_p12.bed
-    python bedtosql.py --sample=${sample} --bed=${bed} --genome=${genome} --out=${out}
-    ./step2_create_db.sh ${out}  
+    	sample=`basename ${bed} | sed -r 's/\..+//'` #Peaks_CB4_BCL6_RK040_vs_Input_RK063_p12
+    	out=${dir}/${genome}/${sample}.sql 
+    	echo ${sample} ${bed}
+	#bed=Peaks_CB4_BCL6_RK040_vs_Input_RK063_p12.bed
+    	python bedtosql.py --sample=${sample} --bed=${bed} --genome=${genome} --out=${out}
+    	./step2_create_db.sh ${out}  
+	break
 done 
