24ae07f2c5f69658473acce348fc84f40fffa58f
Antony Holmes <antony@antonyholmes.dev>
2025-05-27 17:27:12 -0400

    fix: Bug fixes and updates.

--- step3_bigbed.sh
+++ step3_bigbed.sh
@@ -4,6 +4,6 @@ dir=/home/antony/development/data/modules/beds/${genome}/ChIP-seq
 rm ${dir}/bigbed.sql
 
 python trackdbtosql.py --file="/ifs/scratch/cancer/Lab_RDF/ngs/chip_seq/data/human/rdf/hg19/rdf/elodie_dlbcl_cell_lines_29/analysis/hub_elodie_cell_lines_peaks/hg19/trackDb.txt" \
-    --dataset="RDF_29CL" \
+    --dataset="RDF 29CL Peaks" \
     --genome=${genome} \
     --out="${dir}/bigbed.sql"
